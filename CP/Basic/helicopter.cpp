5d3f8b13db90abd4c66890d801d07ef21bfc5bca
gseven-gdv <zerogravitystd@gmail.com>
2023-02-24 11:29:17 -0300

    I'm sad part 2

--- CP/Basic/helicopter.cpp
+++ CP/Basic/helicopter.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 
+char heli(int h, int p, int f, int d) {
+    if (d == -1) {
+        if (p > f && h < f) {
+            return 'S';
+        }
+		else if (p < f && h > f) {
+            return 'N';
+        }
+		else {
+            goto p1;
+        }
+    }
+    else {
+        if (p > f && h < f) {
+            return 'N';
+        }
+		else if (p < f && h > f) {
+            return 'S';
+        }
+		else {
+            goto p2;
+        }
+    }
+
+    p1: 
+        if (p < h) {
+            return 'S';
+        }
+        else if (p > h) {
+            return 'N';
+        }
+    p2: 
+        if (p < h) {
+            return 'N';
+        }
+        else if (p > h) {
+            return 'S';
+        }
+}
+
 int main() {    	
     int h;
     int p;
@@ -8,20 +48,5 @@ int main() {
     
     std::cin >> h >> p >> f >> d;
 
-    if (d == 1) {
-        if (p > h) {
-            std::cout << 'S';
-        }
-        else if (p < h) {
-            std::cout << 'N';
-        }
-    }
-    else if (d == -1) {
-        if (p > h) {
-            std::cout << 'N';
-        }
-        else if (p < h) {
-            std::cout << 'S';
-        }
-    }
+    std::cout << heli(h, p, f, d);
 }
